392f8a22af944dd074d3b6463824114060d48a2a
MichaelSukkiSong <sa77irak@gmail.com>
2024-11-28 19:09:53 +0900

    add: test users can claim

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -37,4 +37,12 @@ contract MerkleAirdrop {
         emit Claim(account, amount);
         i_airdropToken.safeTransfer(account, amount);
     }
+
+    function getMerkeRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
+
+    function getAirDropToken() external view returns (IERC20) {
+        return i_airdropToken;
+    }
 }
